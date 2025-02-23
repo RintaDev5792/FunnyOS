eb9270af0338a904eb7b33b3459cfa9161bdc397
RintaDev <93689788+RintaDev5792@users.noreply.github.com>
2025-02-23 16:17:46 -0500

    Delete compileToInstaller.bat

--- compileToInstaller.bat
+++
@@ -1,5 +0,0 @@
-pdc -s C:\Users\omega\Documents\PlaydateSDK\Games\FunnyOS-Installer C:\Users\omega\Documents\PlaydateSDK\Disk\Games\FunnyOS-Installer.pdx
-pdc -s C:\Users\omega\Documents\PlaydateSDK\Games\FunnyOS C:\Users\omega\Documents\PlaydateSDK\Disk\System\Launcher.pdx
-xcopy /E C:\Users\omega\Documents\PlaydateSDK\Disk\System\Launcher.pdx C:\Users\omega\Documents\PlaydateSDK\Disk\Games\FunnyOS-Installer.pdx\FunnyOS
-rename C:\Users\omega\Documents\PlaydateSDK\Disk\Games\FunnyOS-Installer.pdx\FunnyOS\pdxinfo "-dxinfo"
-pause
