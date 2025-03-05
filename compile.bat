a2704cc1035f982b92729f743290c5170f5bd9d0
RintaDev <93689788+RintaDev5792@users.noreply.github.com>
2025-03-04 20:59:25 -0500

    Revert "Merge branch 'FunnyOS-2' of https://github.com/RintaDev5792/FunnyOS into FunnyOS-2"

---
+++ compile.bat
@@ -0,0 +1,3 @@
+del C:\Users\omega\Documents\PlaydateSDK\Disk\System\Launcher.pdx\music\DWH-Flutterbi-Instrumental.pda
+pdc -s . ..\..\Disk\System\Launcher.pdx
+pause
--- compile.command
+++
@@ -1,2 +0,0 @@
-
-pdc /Users/zel/Developer/PlaydateSDK/Games/FunnyOS\ 2 /Users/zel/Developer/PlaydateSDK/Disk/System/Launchers/FunnyOS2
